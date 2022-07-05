5ffe35971631c2b772482079f26356ba17005b8b
mpobletemori <mpobletemori@gmail.com>
2022-07-04 21:08:26 -0500

    creacion ms-cursos

---
+++ k8s-springboot-parent/ms-cursos/src/main/java/pe/mpobletemori/springcloud/ms/cursos/MsCursosApplication.java
@@ -0,0 +1,13 @@
+package pe.mpobletemori.springcloud.ms.cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCursosApplication.class, args);
+	}
+
+}
---
+++ k8s-springboot-parent/ms-cursos/src/test/java/pe/mpobletemori/springcloud/ms/cursos/MsCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.mpobletemori.springcloud.ms.cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
