246150a6dd162c1762259c936f632e38ce1e4944
mpobletemori <mpobletemori@gmail.com>
2022-09-13 20:55:12 -0500

    Configurar resource Server

--- k8s-springboot-parent/ms-usuarios/src/main/java/pe/mpobletemori/springcloud/ms/usuarios/SecurityConfig.java
+++ k8s-springboot-parent/ms-usuarios/src/main/java/pe/mpobletemori/springcloud/ms/usuarios/SecurityConfig.java
@@ -1,6 +1,7 @@
 package pe.mpobletemori.springcloud.ms.usuarios;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,14 +13,24 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http.authorizeRequests().anyRequest().authenticated()
+        http.authorizeRequests()
+                //Configurar resource Server
+                .antMatchers("/authorized").permitAll()
+                //configurar acceso por scope
+                .antMatchers(HttpMethod.GET,"/","/{id}").hasAnyAuthority("SCOPE_read","SCOPE_write")
+                .antMatchers(HttpMethod.POST,"/").hasAuthority("SCOPE_write")
+                .antMatchers(HttpMethod.PUT,"/{id}").hasAuthority("SCOPE_write")
+                .antMatchers(HttpMethod.DELETE,"/{id}").hasAuthority("SCOPE_write")
+                .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .oauth2Login(oauth2Login->oauth2Login.loginPage("/oauth2/authorization/ms-usuarios-client"))
                 .oauth2Client(Customizer.withDefaults())
-                .build();
+                .oauth2ResourceServer().jwt();
+
+        return http.build();
     }
 
-    
+
 }
