04560a4851ca760fde79bf10355d430e0e79d294
mpobletemori <mpobletemori@gmail.com>
2022-09-13 23:43:37 -0500

    aplicando cambios

--- k8s-springboot-parent/ms-oauth/src/main/java/pe/mpobletemori/springcloud/ms/oauth/SecurityConfig.java
+++ k8s-springboot-parent/ms-oauth/src/main/java/pe/mpobletemori/springcloud/ms/oauth/SecurityConfig.java
@@ -12,9 +12,11 @@ import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
@@ -35,6 +37,9 @@ import org.springframework.security.web.authentication.LoginUrlAuthenticationEnt
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private Environment environment;
 
     @Bean
     @Order(1)
@@ -81,6 +86,7 @@ public class SecurityConfig {
     @Bean
     public RegisteredClientRepository registeredClientRepository() {
         String cliendId="usuarios-client";
+        String urlMsUsuarios = this.environment.getProperty("LB_USUARIOS_URI");
         RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
                 .clientId(cliendId)
                 .clientSecret("{noop}12345")
@@ -88,8 +94,8 @@ public class SecurityConfig {
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .redirectUri("http://127.0.0.1:8001/login/oauth2/code/ms-"+cliendId)
-                .redirectUri("http://127.0.0.1:8001/authorized")
+                .redirectUri(urlMsUsuarios + "/login/oauth2/code/ms-"+cliendId)
+                .redirectUri(urlMsUsuarios + "/authorized")
                 .scope(OidcScopes.OPENID)
                 .scope("read")
                 .scope("write")
